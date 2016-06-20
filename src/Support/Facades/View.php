e5adfd641e501e9a36839a6a97aab0e1d347ab6b
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2016-06-20 20:15:37 +0200

    Add the method share() to View Facade

--- src/Support/Facades/View.php
+++ src/Support/Facades/View.php
@@ -2,6 +2,7 @@
 
 namespace Nova\Support\Facades;
 
+use Nova\View\View as Renderer;
 use Nova\Support\Facades\Facade;
 
 
@@ -10,6 +11,20 @@ use Nova\Support\Facades\Facade;
  */
 class View extends Facade
 {
+    /**
+     * Add a key / value pair to the shared View data.
+     *
+     * Shared View data is accessible to every View created by the application.
+     *
+     * @param  string  $key
+     * @param  mixed   $value
+     * @return void
+     */
+    public static function share($key, $value)
+    {
+        Renderer::share($key, $value);
+    }
+
     /**
      * Get the registered name of the component.
      *
