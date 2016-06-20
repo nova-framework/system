dd94c1df28ab224e3eb36e0e87d8fde5a95602ff
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2016-06-20 15:25:01 +0200

    Remove the Legacy Support on Nova\Core\View

--- src/Core/View.php
+++ src/Core/View.php
@@ -21,17 +21,6 @@ use Response;
 class View extends BaseView
 {
     /**
-     * @var array Array of legacy BaseView instances
-     */
-    private static $legacyItems = array();
-
-    /**
-     * @var array Array of legacy HTTP headers
-     */
-    private static $legacyHeaders = array();
-
-
-    /**
      * Constructor
      * @param mixed $path
      * @param array $data
@@ -71,98 +60,4 @@ class View extends BaseView
 
         return new View($view, $path, $data);
     }
-
-    //--------------------------------------------------------------------
-    // Legacy API Methods
-    //--------------------------------------------------------------------
-
-    /**
-     * Return the stored (legacy) instances.
-     *
-     * @return array
-     */
-    public static function getLegacyItems()
-    {
-        return static::$legacyItems;
-    }
-
-    /**
-     * Return the legacy Headers.
-     *
-     * @return array
-     */
-    public static function getLegacyHeaders()
-    {
-        return static::$legacyHeaders;
-    }
-
-    /**
-     * Add the HTTP header(s) to the legacy Headers array.
-     *
-     * @param  string  $header HTTP header text
-     */
-    protected static function addLegacyHeaders($headers)
-    {
-        if(! is_array($headers)) $headers = array($headers);
-
-        foreach ($headers as $header) {
-            list($key, $value) = explode(' ', $header, 1);
-
-            $key = ltrim($key, ':');
-
-            static::$legacyHeaders[$key] = trim($value);
-        }
-    }
-
-    /**
-     * Magic Method for handling dynamic functions.
-     *
-     * @param  string  $method
-     * @param  array   $params
-     * @return void|mixed
-     */
-    public static function __callStatic($method, $params)
-    {
-        // Process the legacy Headers management.
-        switch ($method) {
-            case 'addHeader':
-            case 'addHeaders':
-                return call_user_func_array(array(static::class, 'addLegacyHeaders'), $params);
-
-            case 'sendHeaders':
-                return null;
-
-            default:
-                break;
-        }
-
-        // The called Class; for getting a View instance.
-        $className = static::class;
-
-        // Flag for fetching the View rendering output.
-        $shouldFetch = false;
-
-        // Prepare the required information.
-        if ($method == 'fetch') {
-            $shouldFetch = true;
-        } else if ($method == 'render') {
-            // Nothing to do; there is no Headers sending.
-        } else if ($method == 'renderTemplate') {
-            $className = Template::class;
-        } else {
-            // No valid Compat Method found; go out.
-            return null;
-        }
-
-        // Create a View instance, using the current Class and the given parameters.
-        $instance = call_user_func_array(array($className, 'make'), $params);
-
-        if ($shouldFetch) {
-            // Render the object and return the captured output.
-            return $instance->fetch();
-        }
-
-        array_push(static::$legacyItems, $instance);
-    }
-
 }
