fc35c5fa19663cede8970f1837faa499edd6c140
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2016-06-20 13:13:03 +0200

    Adjust the Version on Nova\Foundation\Application

--- src/Foundation/Application.php
+++ src/Foundation/Application.php
@@ -44,7 +44,7 @@ class Application extends Container implements HttpKernelInterface, TerminableIn
      *
      * @var string
      */
-    const VERSION = '3.59.1';
+    const VERSION = '4.0-dev';
 
     /**
      * Indicates if the application has "booted".
