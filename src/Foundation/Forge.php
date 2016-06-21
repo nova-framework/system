95c22ec03d0566341386ff69b50080c3e14fc8a3
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2016-06-21 16:37:31 +0200

    Initial implementation of Foundation\Forge

---
+++ src/Foundation/Forge.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Nova\Foundation;
+
+use Nova\Console\Application as ConsoleApplication;
+
+
+class Forge
+{
+    /**
+     * The application instance.
+     *
+     * @var \Nova\Foundation\Application
+     */
+    protected $app;
+
+    /**
+     * The forge console instance.
+     *
+     * @var  \Nova\Console\Application
+     */
+    protected $forge;
+
+    /**
+     * Create a new forge command runner instance.
+     *
+     * @param  \Nova\Foundation\Application  $app
+     * @return void
+     */
+    public function __construct(Application $app)
+    {
+        $this->app = $app;
+    }
+
+    /**
+     * Get the forge console instance.
+     *
+     * @return \Nova\Console\Application
+     */
+    protected function getForge()
+    {
+        if (! is_null($this->forge)) return $this->forge;
+
+        $this->app->loadDeferredProviders();
+
+        $this->forge = ConsoleApplication::make($this->app);
+
+        return $this->forge->boot();
+    }
+
+    /**
+     * Dynamically pass all missing methods to console forge.
+     *
+     * @param  string  $method
+     * @param  array   $parameters
+     * @return mixed
+     */
+    public function __call($method, $parameters)
+    {
+        return call_user_func_array(array($this->getForge(), $method), $parameters);
+    }
+
+}
