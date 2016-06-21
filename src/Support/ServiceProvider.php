7c1c7c310ef97d45d3520df2285db669c9fa6e96
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2016-06-21 18:29:29 +0200

    Improve Nova\Support\ServiceProvider

--- src/Support/ServiceProvider.php
+++ src/Support/ServiceProvider.php
@@ -83,6 +83,16 @@ abstract class ServiceProvider
     }
 
     /**
+     * Get the events that trigger this service provider to register.
+     *
+     * @return array
+     */
+    public function when()
+    {
+        return array();
+    }
+    
+    /**
      * Determine if the provider is deferred.
      *
      * @return bool
