3de25804de1820dc56e4320070dbc5cf3599089f
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2016-06-21 23:56:23 +0200

    Update the Controller generator

--- src/Routing/Generators/ControllerGenerator.php
+++ src/Routing/Generators/ControllerGenerator.php
@@ -134,9 +134,9 @@ class ControllerGenerator
         if (count($segments) > 1) {
             $namespace = implode('\\', array_slice($segments, 0, -1));
 
-            return str_replace('{{namespace}}', ' namespace '.$namespace.';', $stub);
+            return str_replace('{{namespace}}', 'namespace '.$namespace.';', $stub);
         } else {
-            return str_replace('{{namespace}}', 'App\Controllers', $stub);
+            return str_replace('{{namespace}}', 'namespace App\Controllers', $stub);
         }
     }
 
@@ -151,7 +151,7 @@ class ControllerGenerator
     {
         $stubs = $this->getMethodStubs($options);
 
-        $methods = implode(PHP_EOL.PHP_EOL, $stubs);
+        $methods = implode(PHP_EOL, $stubs);
 
         return str_replace('{{methods}}', $methods, $stub);
     }
