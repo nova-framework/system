3c8ee8de196b273aebdfa6155c65f89119a635fb
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2016-06-22 01:13:57 +0200

    Add the Forge Facade

---
+++ src/Support/Facades/Forge.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Nova\Support\Facades;
+
+/**
+ * @see \Nova\Foundation\Forge
+ */
+class Forge extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor() { return 'forge'; }
+
+}
