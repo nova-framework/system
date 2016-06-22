46de3ab65e4c00cc08ce9b0dbeeb7ea474afe209
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2016-06-22 20:56:02 +0200

    Add the Redis and Schema Facades

---
+++ src/Support/Facades/Redis.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Nova\Support\Facades;
+
+/**
+ * @see \Illuminate\Redis\Database
+ */
+class Redis extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor() { return 'redis'; }
+
+}
