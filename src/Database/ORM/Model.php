c3f6f3f5ab7fcbb5c96144b111d73c7f470869bc
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2016-06-22 17:15:12 +0200

    Improve Nova\Database\ORM\Model

--- src/Database/ORM/Model.php
+++ src/Database/ORM/Model.php
@@ -2353,7 +2353,7 @@ abstract class Model implements ArrayAccess, ArrayableInterface, JsonableInterfa
         // If the "attribute" exists as a method on the model, we will just assume
         // it is a relationship and will load and return results from the query
         // and hydrate the relationship's value on the "relationships" array.
-        $camelKey = camel_case($key);
+        $camelKey = Inflector::camelize($key);
 
         if (method_exists($this, $camelKey))
         {
