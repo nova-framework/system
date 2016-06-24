17bd17ca197e7e642575111d6e2ca7da32dd6dc4
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2016-06-24 15:06:40 +0200

    Adjust helpers/functions.php

--- helpers/functions.php
+++ helpers/functions.php
@@ -53,7 +53,8 @@ function app($make = null)
  */
 function app_path($path = '')
 {
-    return app('path').($path ? DS .$path : $path);
+    //return app('path') .($path ? DS .$path : $path);
+    return APPDIR .($path ? DS .$path : $path);
 }
 
 /**
