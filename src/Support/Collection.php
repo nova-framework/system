d379b9f8a08220ac0009266a7dd4c9aa0a5be600
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2016-06-29 23:44:15 +0200

    Improve Nova\Support\Collection

--- src/Support/Collection.php
+++ src/Support/Collection.php
@@ -121,6 +121,22 @@ class Collection implements ArrayAccess, ArrayableInterface, Countable, Iterator
     }
 
     /**
+     * Filter items by the given key value pair.
+     *
+     * @param  string  $key
+     * @param  mixed  $value
+     * @param  bool  $strict
+     * @return static
+     */
+    public function where($key, $value, $strict = true)
+    {
+        return $this->filter(function ($item) use ($key, $value, $strict)
+        {
+            return ($strict ? (data_get($item, $key) === $value) : (data_get($item, $key) == $value));
+        });
+    }
+
+    /**
      * Get the first item from the collection.
      *
      * @param  \Closure   $callback
