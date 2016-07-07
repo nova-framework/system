570a5e530e5af77cb8f2a9a5a2a1ca0fbde5954e
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2016-07-07 16:29:13 +0200

    Move the templating support from App\Routing\Controller

--- src/Routing/Controller.php
+++ src/Routing/Controller.php
@@ -15,30 +15,9 @@ use Closure;
 abstract class Controller
 {
     /**
-     * The Module name.
-     *
-     * @var string|null
-     */
-    private $module = null;
-
-    /**
-     * The Default View.
-     *
-     * @var string
-     */
-    private $defaultView;
-
-    /**
-     * The currently used Template.
-     *
-     * @var string
-     */
-    protected $template;
-
-    /**
      * The currently used Layout.
      *
-     * @var string
+     * @var mixed
      */
     protected $layout;
 
@@ -263,99 +242,27 @@ abstract class Controller
             return $response;
         }
 
-        // If the response is returned from the controller action is a View instance
-        // and it is not marked as Layout, we will assume we want to render it on the
-        // default templated environment, setup via the current controller properties.
-        else if ($response instanceof View) {
-            if (is_string($this->layout) && ! $response->isLayout()) {
-                $response = app('template')
-                    ->make($this->layout, $this->template)
-                    ->with('content', $response->fetch());
-            }
-        }
-
         // If no response is returned from the controller action and a layout is being
         // used we will assume we want to just return the Layout view as any nested
         // Views were probably bound on this view during this Controller actions.
-        else if (is_null($response)) {
-            if ($this->layout instanceof View) {
-                $response = $this->layout;
-            }
+        else if (is_null($response) && ($this->layout instanceof View)) {
+            $response = $this->layout;
         }
 
-        // Create a proper Response and return it.
+        // Process the Response and return it.
+        return $this->processResponse($response);
+    }
+
+    /**
+     * Process the response given by the controller action.
+     *
+     * @param mixed $response
+     *
+     * @return mixed
+     */
+    protected function processResponse($response)
+    {
         return $response;
-    }
-
-    /**
-     * @param  string $title
-     *
-     * @return \Nova\Routing\Controller
-     */
-    protected function title($title)
-    {
-        app('view')->share('title', $title);
-    }
-
-    /**
-     * Return a default View instance.
-     *
-     * @return \Nova\View\View
-     */
-    protected function getView(array $data = array())
-    {
-        if(! isset($this->defaultView)) {
-            list(, $caller) = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 2);
-
-            $this->setupDefaultView($caller['function']);
-        }
-
-        return app('view')->make($this->defaultView, $data, $this->module);
-    }
-
-    /**
-     * @return void
-     */
-    private function setupDefaultView($method)
-    {
-        // Prepare the View Path using the Controller's full Name including its namespace.
-        $classPath = str_replace('\\', '/', static::class);
-
-        if (preg_match('#^App/Controllers/(.*)$#i', $classPath, $matches)) {
-            // The View is in default App sub-directory.
-            $this->defaultView = $matches[1] .DS .ucfirst($method);
-        } else if (preg_match('#^App/Modules/(.+)/Controllers/(.*)$#i', $classPath, $matches)) {
-             // The View is in a Module sub-directory.
-            $this->module = $matches[1];
-
-            $this->defaultView = $matches[2] .DS .ucfirst($method);
-        } else {
-            throw new BadMethodCallException('Invalid Controller: ' .static::class);
-        }
-    }
-
-    /**
-     * @return string|null
-     */
-    protected function getModule()
-    {
-        return $this->module;
-    }
-
-    /**
-     * @return mixed
-     */
-    protected function getTemplate()
-    {
-        return $this->template;
-    }
-
-    /**
-     * @return mixed
-     */
-    protected function getLayout()
-    {
-        return $this->layout;
     }
 
     /**
