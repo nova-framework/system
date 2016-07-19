86bf0ac2f3452ebc5702b911429728f524ca986a
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2016-07-19 09:21:17 +0200

    Implementation of Nova\Remote

---
+++ src/Remote/RemoteServiceProvider.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Nova\Remote;
+
+use Nova\Support\ServiceProvider;
+
+
+class RemoteServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = true;
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bindShared('remote', function($app)
+        {
+            return new RemoteManager($app);
+        });
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array('remote');
+    }
+
+}
