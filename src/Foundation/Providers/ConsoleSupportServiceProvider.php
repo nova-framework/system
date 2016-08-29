760349c7d1f2874306932261f9b15d1f3b4c0d75
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2016-08-29 19:56:18 +0200

    Adjust Nova\Foundation\Providers\ConsoleSupportServiceProvider

--- src/Foundation/Providers/ConsoleSupportServiceProvider.php
+++ src/Foundation/Providers/ConsoleSupportServiceProvider.php
@@ -22,7 +22,6 @@ class ConsoleSupportServiceProvider extends ServiceProvider
         'Nova\Foundation\Providers\RouteListServiceProvider',
         'Nova\Foundation\Providers\ServerServiceProvider',
         'Nova\Foundation\Providers\TinkerServiceProvider',
-        'Nova\Queue\FailConsoleServiceProvider',
     );
 
     /**
