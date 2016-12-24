3c6e8ea75f750c025251542e143c4af8a6a64bda
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2016-12-24 11:31:55 +0200

    Adjust scripts/helpers.php and composer.json

--- scripts/helpers.php
+++ scripts/helpers.php
@@ -197,16 +197,16 @@ if (! function_exists('app_path'))
 
 if (! function_exists('base_path'))
 {
-        /**
-         * Get the path to the base of the install.
-         *
-         * @param  string  $path
-         * @return string
-         */
-        function base_path($path = '')
-        {
-                return app()->make('path.base') .($path ? DS .$path : $path);
-        }
+    /**
+     * Get the path to the base of the install.
+     *
+     * @param  string  $path
+     * @return string
+     */
+    function base_path($path = '')
+    {
+        return app()->make('path.base') .($path ? DS .$path : $path);
+    }
 }
 
 if (! function_exists('storage_path'))
