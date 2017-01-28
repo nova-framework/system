ad2201e366e8498db815a5a062e674f464175531
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-01-28 02:10:00 +0200

    Overall improvements

---
+++ src/Support/HtmlString.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Nova\Support;
+
+use Nova\Support\Contracts\HtmlableInterface;
+
+
+class HtmlString implements HtmlableInterface
+{
+    /**
+     * The HTML string.
+     *
+     * @var string
+     */
+    protected $html;
+
+    
+    /**
+     * Create a new HTML string instance.
+     *
+     * @param  string  $html
+     * @return void
+     */
+    public function __construct($html)
+    {
+        $this->html = $html;
+    }
+
+    /**
+     * Get the the HTML string.
+     *
+     * @return string
+     */
+    public function toHtml()
+    {
+        return $this->html;
+    }
+
+    /**
+     * Get the the HTML string.
+     *
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->toHtml();
+    }
+}
---
+++ src/Validation/ValidatesWhenResolvedTrait.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Nova\Validation;
+
+use Nova\Validation\ValidationException;
+use Nova\Validation\UnauthorizedException;
+
+/**
+ * Provides default implementation of ValidatesWhenResolvedInterface contract.
+ */
+trait ValidatesWhenResolvedTrait
+{
+    /**
+     * Validate the class instance.
+     *
+     * @return void
+     */
+    public function validate()
+    {
+        $instance = $this->getValidatorInstance();
+
+        if (! $this->passesAuthorization()) {
+            $this->failedAuthorization();
+        } elseif (! $instance->passes()) {
+            $this->failedValidation($instance);
+        }
+    }
+
+    /**
+     * Get the validator instance for the request.
+     *
+     * @return \Nova\Validation\Validator
+     */
+    protected function getValidatorInstance()
+    {
+        return $this->validator();
+    }
+
+    /**
+     * Handle a failed validation attempt.
+     *
+     * @param  \Nova\Validation\Validator  $validator
+     * @return mixed
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        throw new ValidationException($validator);
+    }
+
+    /**
+     * Determine if the request passes the authorization check.
+     *
+     * @return bool
+     */
+    protected function passesAuthorization()
+    {
+        if (method_exists($this, 'authorize')) {
+            return $this->authorize();
+        }
+
+        return true;
+    }
+
+    /**
+     * Handle a failed authorization attempt.
+     *
+     * @return mixed
+     */
+    protected function failedAuthorization()
+    {
+        throw new UnauthorizedException;
+    }
+}
