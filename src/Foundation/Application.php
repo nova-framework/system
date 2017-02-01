9c271ad36668292e6509b04ecbb9393ca375b980
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-02-01 17:01:52 +0200

    Update the Kernel version

--- src/Foundation/Application.php
+++ src/Foundation/Application.php
@@ -36,7 +36,7 @@ class Application extends Container implements HttpKernelInterface, TerminableIn
      *
      * @var string
      */
-    const VERSION = '3.75.6';
+    const VERSION = '3.75.7';
 
     /**
      * Indicates if the application has "booted".
