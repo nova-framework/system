1c9017bc74868530f452cfe25154d0783059c905
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-02-11 12:58:26 +0200

    Improve Nova\Support\ServiceProvider

--- src/Support/ServiceProvider.php
+++ src/Support/ServiceProvider.php
@@ -38,19 +38,11 @@ abstract class ServiceProvider
     }
 
     /**
-     * Bootstrap the application events.
-     *
-     * @return void
-     */
-    public function boot() {}
-
-    /**
      * Register the service provider.
      *
      * @return void
      */
     abstract public function register();
-
 
     /**
      * Get the services provided by the provider.
@@ -80,5 +72,21 @@ abstract class ServiceProvider
     public function isDeferred()
     {
         return $this->defer;
+    }
+
+    /**
+     * Dynamically handle missing method calls.
+     *
+     * @param  string  $method
+     * @param  array  $parameters
+     * @return mixed
+     */
+    public function __call($method, $parameters)
+    {
+        if ($method == 'boot') {
+            return;
+        }
+
+        throw new BadMethodCallException("Call to undefined method [{$method}]");
     }
 }
