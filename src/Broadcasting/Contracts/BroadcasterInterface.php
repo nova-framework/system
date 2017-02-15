d38965eb27299166c2dd335fd2c333aa2e52594f
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-02-15 10:23:06 +0200

    Initial implementation of the Broadcasting Service

---
+++ src/Broadcasting/Contracts/BroadcasterInterface.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Nova\Broadcasting\Contracts;
+
+
+interface BroadcasterInterface
+{
+    /**
+     * Broadcast the given event.
+     *
+     * @param  array  $channels
+     * @param  string  $event
+     * @param  array  $payload
+     * @return void
+     */
+    public function broadcast(array $channels, $event, array $payload = array());
+}
---
+++ src/Broadcasting/Contracts/ShouldBroadcastNowInterface.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Nova\Broadcasting\Contracts;
+
+
+interface ShouldBroadcastNowInterface extends ShouldBroadcastInterface
+{
+    //
+}
