9055e5ba33e7308ad8defa900fb61cbd86702f20
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-02-18 14:31:03 +0200

    Implement the Nova\Support\Facades\Template

---
+++ src/Support/Facades/Template.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Nova\Support\Facades;
+
+/**
+ * @see \Nova\View\Compilers\TemplateCompiler
+ */
+class Template extends Facade
+{
+
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+    return static::$app['view']->getEngineResolver()->resolve('template')->getCompiler();
+    }
+
+}
