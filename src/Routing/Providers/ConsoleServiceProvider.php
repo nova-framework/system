ebdf10aea2da70be65105e0353f9f75286fdea4a
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-02-22 20:47:20 +0200

    Initial implementation of the Controller generator in Forge

---
+++ src/Routing/Providers/ConsoleServiceProvider.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Nova\Routing\Providers;
+
+use Nova\Routing\Console\ControllerMakeCommand;
+use Nova\Routing\Generators\ControllerGenerator;
+use Nova\Support\ServiceProvider;
+
+
+class ConsoleServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = true;
+
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bindShared('command.controller.make', function($app)
+        {
+            $path = $app['path'] .DS .'Controllers';
+
+            $generator = new ControllerGenerator($app['files']);
+
+            return new ControllerMakeCommand($generator, $path);
+        });
+
+        $this->commands('command.controller.make');
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array(
+            'command.controller.make'
+        );
+    }
+
+}
