e9a5b351deb82e7b8962bc2ebad674faed79b3c5
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-06-30 00:04:28 +0300

    Improve the Routing

--- src/Routing/Router.php
+++ src/Routing/Router.php
@@ -68,13 +68,6 @@ class Router
 	protected $middlewareGroups = array();
 
 	/**
-	 * The controller inspector instance.
-	 *
-	 * @var \Nova\Routing\ControllerInspector
-	 */
-	protected $inspector;
-
-	/**
 	 * The registered route value binders.
 	 *
 	 * @var array
@@ -228,87 +221,6 @@ class Router
 	public function match($methods, $uri, $action)
 	{
 		return $this->addRoute(array_map('strtoupper', (array) $methods), $uri, $action);
-	}
-
-	/**
-	 * Register an array of controllers with wildcard routing.
-	 *
-	 * @param  array  $controllers
-	 * @return void
-	 */
-	public function controllers(array $controllers)
-	{
-		foreach ($controllers as $uri => $name) {
-			$this->controller($uri, $name);
-		}
-	}
-
-	/**
-	 * Route a controller to a URI with wildcard routing.
-	 *
-	 * @param  string  $uri
-	 * @param  string  $controller
-	 * @param  array   $names
-	 * @return void
-	 */
-	public function controller($uri, $controller, $names = array())
-	{
-		$prepended = $controller;
-
-		// First, we will check to see if a controller prefix has been registered in
-		// the route group. If it has, we will need to prefix it before trying to
-		// reflect into the class instance and pull out the method for routing.
-		if (! empty($this->groupStack)) {
-			$prepended = $this->prependGroupUses($controller);
-		}
-
-		$routable = $this->getInspector()->getRoutable($prepended, $uri);
-
-		// When a controller is routed using this method, we use Reflection to parse
-		// out all of the routable methods for the controller, then register each
-		// route explicitly for the developers, so reverse routing is possible.
-		foreach ($routable as $method => $routes) {
-			foreach ($routes as $route) {
-				$this->registerInspected($route, $controller, $method, $names);
-			}
-		}
-
-		$this->addFallthroughRoute($controller, $uri);
-	}
-
-	/**
-	 * Register an inspected controller route.
-	 *
-	 * @param  array   $route
-	 * @param  string  $controller
-	 * @param  string  $method
-	 * @param  array   $names
-	 * @return void
-	 */
-	protected function registerInspected($route, $controller, $method, &$names)
-	{
-		$action = array('uses' => $controller.'@'.$method);
-
-		// If a given controller method has been named, we will assign the name to the
-		// controller action array, which provides for a short-cut to method naming
-		// so you don't have to define an individual route for these controllers.
-		$action['as'] = array_get($names, $method);
-
-		$this->{$route['verb']}($route['uri'], $action);
-	}
-
-	/**
-	 * Add a fallthrough route for a controller.
-	 *
-	 * @param  string  $controller
-	 * @param  string  $uri
-	 * @return void
-	 */
-	protected function addFallthroughRoute($controller, $uri)
-	{
-		$missing = $this->any($uri.'/{_missing}', $controller.'@missingMethod');
-
-		$missing->where('_missing', '(.*)');
 	}
 
 	/**
@@ -1221,16 +1133,6 @@ class Router
 	public function getRoutes()
 	{
 		return $this->routes;
-	}
-
-	/**
-	 * Get a controller inspector instance.
-	 *
-	 * @return \Nova\Routing\ControllerInspector
-	 */
-	public function getInspector()
-	{
-		return $this->inspector ?: $this->inspector = new ControllerInspector;
 	}
 
 	/**
