c8512e90f56a174585f9683a6a9f1e165adbe680
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-06-30 19:00:37 +0300

    Overall improvements

--- src/Foundation/Console/Kernel.php
+++ src/Foundation/Console/Kernel.php
@@ -49,7 +49,6 @@ class Kernel implements KernelInterface
 	 */
 	protected $bootstrappers = array(
 		'Nova\Foundation\Bootstrap\LoadConfiguration',
-		'Nova\Foundation\Bootstrap\ConfigureLogging',
 		'Nova\Foundation\Bootstrap\HandleExceptions',
 		'Nova\Foundation\Bootstrap\BootApplication',
 		'Nova\Foundation\Bootstrap\SetRequestForConsole',
