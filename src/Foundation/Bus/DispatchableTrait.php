82327d06d03b60aef11d39af09a4915371fe0021
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-06-30 20:28:31 +0300

    Improve the Foundation

---
+++ src/Foundation/Bus/DispatchableTrait.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Nova\Foundation\Bus;
+
+
+trait DispatchableTrait
+{
+	/**
+	 * Dispatch the job with the given arguments.
+	 *
+	 * @return \Nova\Foundation\Bus\PendingDispatch
+	 */
+	public static function dispatch()
+	{
+		return new PendingDispatch(new static(...func_get_args()));
+	}
+}
---
+++ src/Foundation/Events/DispatchableTrait.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Nova\Foundation\Events;
+
+
+trait DispatchableTrait
+{
+	/**
+	 * Dispatch the event with the given arguments.
+	 *
+	 * @return void
+	 */
+	public static function dispatch()
+	{
+		return event(new static(...func_get_args()));
+	}
+
+	/**
+	 * Broadcast the event with the given arguments.
+	 *
+	 * @return \Illuminate\Broadcasting\PendingBroadcast
+	 */
+	public static function broadcast()
+	{
+		return broadcast(new static(...func_get_args()));
+	}
+}
