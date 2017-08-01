759cf4c81020a0b785d571d08418af0b73f892a1
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-08-01 15:41:20 +0300

    Update the language files

--- src/Language/FR/messages.php
+++ src/Language/FR/messages.php
@@ -3,11 +3,9 @@
 return array (
   '&laquo; Previous' => '',
   'Next &raquo;' => '',
-  'Reset Password' => '',
   'These credentials do not match our records.' => '',
   'This action is unauthorized.' => '',
   'Too many login attempts. Please try again in {0} seconds.' => '',
   'User Login' => '',
   'User Registration' => '',
-  'Your Password Reset Link' => '',
 );
--- src/Language/RU/messages.php
+++ src/Language/RU/messages.php
@@ -3,11 +3,9 @@
 return array (
   '&laquo; Previous' => 'Назад',
   'Next &raquo;' => 'Дальше',
-  'Reset Password' => '',
   'These credentials do not match our records.' => 'Эти учетные данные не соответствуют нашим записям.',
   'This action is unauthorized.' => 'Это действие несанкционировано',
   'Too many login attempts. Please try again in {0} seconds.' => 'Слишком много попыток входа. Повторите попытку через {0} секунд.',
   'User Login' => 'Вход',
   'User Registration' => 'Регистрация пользователя',
-  'Your Password Reset Link' => '',
 );
