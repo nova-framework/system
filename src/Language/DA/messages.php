7a18436947d0ec85421ab9299e197f5fb131ee37
DonWagner <michael@soerensens.dk>
2017-08-02 09:22:18 +0200

    [4.0] Danish Language update

--- src/Language/DA/messages.php
+++ src/Language/DA/messages.php
@@ -1,11 +1,11 @@
 <?php
 
 return array (
-  '&laquo; Previous' => '',
-  'Next &raquo;' => '',
-  'These credentials do not match our records.' => '',
-  'This action is unauthorized.' => '',
-  'Too many login attempts. Please try again in {0} seconds.' => '',
-  'User Login' => '',
-  'User Registration' => '',
+  '&laquo; Previous' => '&laquo; Sidste',
+  'Next &raquo;' => 'Næste &raquo;',
+  'These credentials do not match our records.' => 'Disse oplysninger passer ikke til vores.',
+  'This action is unauthorized.' => 'Denne handling er uautoriseret.',
+  'Too many login attempts. Please try again in {0} seconds.' => 'Du har forsøgt at logge ind for mange gange. Prøv igen om {0} sekunder.',
+  'User Login' => 'Bruger login',
+  'User Registration' => 'Brugerregistrering',
 );
