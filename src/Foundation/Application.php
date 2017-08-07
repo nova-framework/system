9024d022ffbab8af0476924a7d7dbdc35874a182
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-08-07 18:05:40 +0300

    Remove the RainCaptcha support

--- src/Foundation/Application.php
+++ src/Foundation/Application.php
@@ -37,7 +37,7 @@ class Application extends Container implements HttpKernelInterface, TerminableIn
      *
      * @var string
      */
-    const VERSION = '3.77.39';
+    const VERSION = '3.77.40';
 
     /**
      * Indicates if the application has "booted".
