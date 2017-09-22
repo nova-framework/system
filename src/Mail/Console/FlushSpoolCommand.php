cc1cd4189ae39f2fa4e9a22b3e59fac32a85d62f
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-09-22 13:31:17 +0300

    Small change

--- src/Mail/Console/FlushSpoolCommand.php
+++ src/Mail/Console/FlushSpoolCommand.php
@@ -23,7 +23,7 @@ class FlushSpoolCommand extends Command
      *
      * @var string
      */
-    protected $description = "Send the messages queued on Mailer Spool";
+    protected $description = "Send the messages queued in Mailer Spool";
 
     /**
      * The event dispatcher instance.
