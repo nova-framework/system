f01315e526ffc4dfba8baa7ab9a553ca611435eb
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-10-25 20:31:59 +0300

    Small changes

--- src/Routing/RoutingServiceProvider.php
+++ src/Routing/RoutingServiceProvider.php
@@ -158,17 +158,17 @@ class RoutingServiceProvider extends ServiceProvider
         });
 
         // For assets from Modules and Themes.
-        $dispatcher->route('(themes|modules)/([^/]+)/assets/(.*)', function (Request $request, $type, $folder, $path) use ($dispatcher)
+        $dispatcher->route('(themes|modules)/([^/]+)/assets/(.*)', function (Request $request, $type, $component, $path) use ($dispatcher)
         {
             $type = (strtolower($type) == 'modules') ? 'Modules' : 'Themes';
 
-            if (strlen($folder) > 3) {
-                $folder = Str::studly($folder);
+            if (strlen($component) > 3) {
+                $component = Str::studly($component);
             } else {
-                $folder = Str::upper($folder);
+                $component = Str::upper($component);
             }
 
-            $path = APPDIR .$type .DS .$folder .DS .'Assets' .DS .str_replace('/', DS, $path);
+            $path = APPDIR .$type .DS .$component .DS .'Assets' .DS .str_replace('/', DS, $path);
 
             return $dispatcher->serve($path, $request);
         });
