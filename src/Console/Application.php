bfd0907bd7eb64e893b75c332ad9fbe7e011f467
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-10-27 14:07:53 +0300

    Small changes

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -66,7 +66,7 @@ class Application extends \Symfony\Component\Console\Application
      */
     public function boot()
     {
-        $path = $this->container['path'] .DS .'Console' .DS .'Bootstrap.php';
+        $path = $this->container['path'] .DS .'Console' .DS .'Forge.php';
 
         if (file_exists($path)) {
             require $path;
