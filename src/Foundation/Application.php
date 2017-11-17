c0415366604e8584ad2791445f90fc4bfb63e760
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-11-17 13:59:28 +0200

    Improve the Modules generator

--- src/Foundation/Application.php
+++ src/Foundation/Application.php
@@ -41,7 +41,7 @@ class Application extends Container implements ResponsePreparerInterface
      *
      * @var string
      */
-    const VERSION = '3.80.11';
+    const VERSION = '3.80.12';
 
     /**
      * Indicates if the application has "booted".
