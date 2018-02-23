ccc17ad38e16044c5e45f83b6423b97793d9c2d9
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2018-02-23 16:20:23 +0200

    Improve the Nova\Broadcasting\Broadcaster

--- src/Broadcasting/Broadcaster.php
+++ src/Broadcasting/Broadcaster.php
@@ -74,12 +74,53 @@ abstract class Broadcaster implements BroadcasterInterface
 
             array_unshift($parameters, $request->user());
 
-            if ($result = call_user_func_array($callback, $parameters)) {
+            // Resolve the Channel handler callable.
+            $handler = $this->makeChannelHandler($callback);
+
+            if ($result = $this->container->call($handler, $parameters)) {
                 return $this->validAuthenticationResponse($request, $result);
             }
         }
 
         throw new AccessDeniedHttpException;
+    }
+
+    /**
+     * Create a Channel handler callable.
+     *
+     * @param  mixed   $handler
+     * @return mixed
+     */
+    public function makeChannelHandler($handler)
+    {
+        if (is_string($handler)) {
+            return $this->createClassHandler($handler);
+        }
+
+        return $handler;
+    }
+
+    /**
+     * Create a class based handler using the IoC container.
+     *
+     * @param  mixed    $handler
+     * @return \Closure
+     */
+    public function createClassHandler($handler)
+    {
+        return function () use ($handler)
+        {
+            list($className, $method) = array_pad(explode('@', $handler, 2), 2, 'join');
+
+            $instance = $this->container->make($className);
+
+            // We will make a callable of the handler instance and a method that should
+            // be called on that instance, then we will pass in the arguments that we
+            // received in this method into this handler class instance's methods.
+            $parameters = func_get_args();
+
+            return call_user_func_array($instance, $parameters);
+        };
     }
 
     /**
