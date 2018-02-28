1af6a29f3eb05cf8bafa23535752c65d98d9156f
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2018-02-28 13:59:27 +0200

    Improve the Modules Service

--- src/Module/ConsoleServiceProvider.php
+++ src/Module/ConsoleServiceProvider.php
@@ -1,6 +1,15 @@
 <?php
 
-namespace Nova\Module\Providers;
+namespace Nova\Module;
+
+use Nova\Module\Console\ModuleListCommand;
+use Nova\Module\Console\ModuleMigrateCommand;
+use Nova\Module\Console\ModuleMigrateRefreshCommand;
+use Nova\Module\Console\ModuleMigrateResetCommand;
+use Nova\Module\Console\ModuleMigrateRollbackCommand;
+use Nova\Module\Console\ModuleMigrateStatusCommand;
+use Nova\Module\Console\ModuleSeedCommand;
+use Nova\Module\Console\ModuleOptimizeCommand;
 
 use Nova\Module\Console\ModuleMakeCommand;
 use Nova\Module\Console\ConsoleMakeCommand;
@@ -17,7 +26,7 @@ use Nova\Module\Console\SeederMakeCommand;
 use Nova\Support\ServiceProvider;
 
 
-class GeneratorServiceProvider extends ServiceProvider
+class ConsoleServiceProvider extends ServiceProvider
 {
     /**
      * Indicates if loading of the Provider is deferred.
@@ -41,24 +50,131 @@ class GeneratorServiceProvider extends ServiceProvider
     public function register()
     {
         $commands = array(
-            'Module',
-            'Console',
-            'Controller',
-            'Event',
-            'Listener',
-            'Middleware',
-            'Model',
-            'Policy',
-            'Provider',
-            'Migration',
-            'Seeder'
+            'ModuleList',
+            'ModuleMigrate',
+            'ModuleMigrateRefresh',
+            'ModuleMigrateReset',
+            'ModuleMigrateRollback',
+            'ModuleMigrateStatus',
+            'ModuleOptimize',
+            'ModuleSeed',
+
+            // Generators
+            'ModuleMake',
+            'ConsoleMake',
+            'ControllerMake',
+            'EventMake',
+            'ListenerMake',
+            'MiddlewareMake',
+            'ModelMake',
+            'PolicyMake',
+            'ProviderMake',
+            'MigrationMake',
+            'SeederMake'
         );
 
         foreach ($commands as $command) {
-            $method = 'register' .$command .'MakeCommand';
-
-            call_user_func(array($this, $method));
+            $this->{'register' .$command .'Command'}();
         }
+    }
+
+    /**
+     * Register the module:list command.
+     */
+    protected function registerModuleListCommand()
+    {
+        $this->app->singleton('command.module.list', function ($app) {
+            return new ModuleListCommand($app['modules']);
+        });
+
+        $this->commands('command.module.list');
+    }
+
+    /**
+     * Register the module:migrate command.
+     */
+    protected function registerModuleMigrateCommand()
+    {
+        $this->app->singleton('command.module.migrate', function ($app) {
+            return new ModuleMigrateCommand($app['migrator'], $app['modules']);
+        });
+
+        $this->commands('command.module.migrate');
+    }
+
+    /**
+     * Register the module:migrate:refresh command.
+     */
+    protected function registerModuleMigrateRefreshCommand()
+    {
+        $this->app->singleton('command.module.migrate.refresh', function ($app) {
+            return new ModuleMigrateRefreshCommand($app['modules']);
+        });
+
+        $this->commands('command.module.migrate.refresh');
+    }
+
+    /**
+     * Register the module:migrate:reset command.
+     */
+    protected function registerModuleMigrateResetCommand()
+    {
+        $this->app->singleton('command.module.migrate.reset', function ($app) {
+            return new ModuleMigrateResetCommand($app['modules'], $app['files'], $app['migrator']);
+        });
+
+        $this->commands('command.module.migrate.reset');
+    }
+
+    /**
+     * Register the module:migrate:rollback command.
+     */
+    protected function registerModuleMigrateRollbackCommand()
+    {
+        $this->app->singleton('command.module.migrate.rollback', function ($app) {
+            return new ModuleMigrateRollbackCommand($app['migrator'], $app['modules']);
+        });
+
+        $this->commands('command.module.migrate.rollback');
+    }
+
+    /**
+     * Register the "status" migration command.
+     *
+     * @return void
+     */
+    protected function registerModuleMigrateStatusCommand()
+    {
+        $this->app->bindShared('command.module.migrate.status', function ($app)
+        {
+            return new ModuleMigrateStatusCommand($app['migrator'], $app['modules']);
+        });
+
+        $this->commands('command.module.migrate.status');
+    }
+
+    /**
+     * Register the module:seed command.
+     */
+    protected function registerModuleSeedCommand()
+    {
+        $this->app->singleton('command.module.seed', function ($app) {
+            return new ModuleSeedCommand($app['modules']);
+        });
+
+        $this->commands('command.module.seed');
+    }
+
+    /**
+     * Register the module:list command.
+     */
+    protected function registerModuleOptimizeCommand()
+    {
+        $this->app->singleton('command.module.optimize', function ($app) {
+            return new ModuleOptimizeCommand($app['modules']);
+        });
+
+        $this->commands('command.module.optimize');
     }
 
     /**
