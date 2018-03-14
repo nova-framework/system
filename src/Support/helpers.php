ec07bfaa777fb0d1ced5075fc13ed03c6122af01
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2018-03-14 02:30:55 +0200

    Small changes

--- src/Support/helpers.php
+++ src/Support/helpers.php
@@ -22,7 +22,7 @@ if (! function_exists('site_url'))
 
         $path = ! empty($parameters) ? array_shift($parameters) : '/';
 
-        $result = preg_replace_callback('#/\{(.*?)\}#', function ($matches) use ($parameters)
+        $path = preg_replace_callback('#/\{(\d+)\}#', function ($matches) use ($parameters)
         {
             list ($capture, $name) = $matches;
 
@@ -30,7 +30,7 @@ if (! function_exists('site_url'))
 
         }, $path);
 
-        return url($result);
+        return url($path);
     }
 }
 
