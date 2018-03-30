a1ca9f4c3e8423e11707c90b76dd38bd4e739582
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2018-03-30 16:55:20 +0300

    Update the composer.json

--- src/Foundation/Application.php
+++ src/Foundation/Application.php
@@ -41,7 +41,7 @@ class Application extends Container implements ResponsePreparerInterface
      *
      * @var string
      */
-    const VERSION = '4.0.29';
+    const VERSION = '4.0.30';
 
     /**
      * Indicates if the application has "booted".
