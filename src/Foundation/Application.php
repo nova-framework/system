b936648e04c6d9b17c95b97abf6968778e2f75d8
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2018-05-23 20:01:06 +0300

    Update the Kernel version

--- src/Foundation/Application.php
+++ src/Foundation/Application.php
@@ -41,7 +41,7 @@ class Application extends Container implements ResponsePreparerInterface
      *
      * @var string
      */
-    const VERSION = '4.0.59';
+    const VERSION = '4.0.58';
 
     /**
      * Indicates if the application has "booted".
