01987fbb08f76f4e3c48768ba1bd4c65ab2f496a
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2018-05-24 02:40:45 +0300

    Improve the Assets Service

--- src/Foundation/Application.php
+++ src/Foundation/Application.php
@@ -41,7 +41,7 @@ class Application extends Container implements ResponsePreparerInterface
      *
      * @var string
      */
-    const VERSION = '4.0.62';
+    const VERSION = '4.0.63';
 
     /**
      * Indicates if the application has "booted".
