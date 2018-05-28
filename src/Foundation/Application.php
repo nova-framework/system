815760dcd35b3ed85322e010ecba7d0ba430cf52
LuckyCyborg <darkstarlinux@gmail.com>
2018-05-29 00:58:48 +0300

    Update Application.php

--- src/Foundation/Application.php
+++ src/Foundation/Application.php
@@ -41,7 +41,7 @@ class Application extends Container implements ResponsePreparerInterface
      *
      * @var string
      */
-    const VERSION = '4.0.71';
+    const VERSION = '4.0.72';
 
     /**
      * Indicates if the application has "booted".
