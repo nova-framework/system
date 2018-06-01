a3bcdb732eea05e1a1ff7a62f49c349934474032
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2018-06-01 04:32:09 +0300

    Update the Symfony components

--- src/Foundation/Application.php
+++ src/Foundation/Application.php
@@ -41,7 +41,7 @@ class Application extends Container implements ResponsePreparerInterface
      *
      * @var string
      */
-    const VERSION = '4.0.73';
+    const VERSION = '4.0.74';
 
     /**
      * Indicates if the application has "booted".
