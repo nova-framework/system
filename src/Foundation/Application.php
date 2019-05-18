cdc8b13026ec8efffcd24af7078009f492a1ff41
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2019-05-18 23:51:43 +0300

    Update the framework version

--- src/Foundation/Application.php
+++ src/Foundation/Application.php
@@ -41,7 +41,7 @@ class Application extends Container implements ResponsePreparerInterface
      *
      * @var string
      */
-    const VERSION = '4.1.13';
+    const VERSION = '4.1.14';
 
     /**
      * Indicates if the application has "booted".
