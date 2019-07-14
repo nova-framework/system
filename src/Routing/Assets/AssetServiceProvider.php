0fd8283ba963b1c0456c3d566a0885bd66952199
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2019-07-14 05:15:45 +0300

    Small changes

--- src/Routing/Assets/AssetServiceProvider.php
+++ src/Routing/Assets/AssetServiceProvider.php
@@ -37,7 +37,7 @@ class AssetServiceProvider extends ServiceProvider
     }
 
     /**
-     * Register the Assets Dispatcher.
+     * Register the Assets Dispatcher instance.
      *
      * @return void
      */
