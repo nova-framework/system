15f5b3ea35fa807e52899a67f3221038de778c12
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2020-09-29 23:49:29 +0300

    Small changes

--- src/Support/ServiceProvider.php
+++ src/Support/ServiceProvider.php
@@ -74,6 +74,7 @@ abstract class ServiceProvider
         // In this method we will register the configuration package for the package
         // so that the configuration options cleanly cascade into the application
         // folder to make the developers lives much easier in maintaining them.
+
         $path = $path ?: $this->guessPackagePath();
 
         // Register the Package Config path.
